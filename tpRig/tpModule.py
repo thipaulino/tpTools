fa2642e2c6b3da887fc60353e21501324bfb8461
tpaulino <tpaulino.com@gmail.com>
2021-04-25 19:44:56 -0700

    - double underscores removed to make the code overwritable at any point - removed unused methods (class ModuleGroupManager still needs to be removed)

--- tpRig/tpModule.py
+++ tpRig/tpModule.py
@@ -1,11 +1,25 @@
 import tpRig.tpNameConvention as tpName
+import tpRig.tpNodeAttributeManager as tpAttr
+reload(tpAttr)
 import maya.cmds as mc
 
 
 class TpModule(object):
 
     def __init__(self, name=None, side=None):
+        """
+        The main function of the module is to:
+        - Standardize groups across the system
+        - Create the module top group
+        - Create and manage top group attributes
+        - Manage module data
+        - Group and organize all created nodes
+
+        :param name:
+        :param side:
+        """
         self.tp_name = tpName.NameConvention()
+        self.module_attribute_manager = None
 
         self.module_name = name
         self.module_side = side
@@ -14,11 +28,11 @@ class TpModule(object):
         self.module_top_group = None
         self.module_data = None
         self.module_control_node = None
+        self.module_control_attribute_list = []
 
         self.module_group_dict = {
             'joint': [],
-            'rig_joint': [],
-            # 'utility_joint': [],
+            'rig_joint': [],  # utility_joint
             'bind_joint': [],
             'geometry': [],
             'rig_geometry': [],
@@ -26,7 +40,9 @@ class TpModule(object):
             'locator': [],
             'utility': [],
             'follicle': [],
-            'curve': []
+            'curve': [],
+            'module': [],
+            'sub_module': []
         }
 
         # module component lists
@@ -40,6 +56,7 @@ class TpModule(object):
         self.geometry_list = []
         self.surface_list = []
         self.locator_list = []
+        self.sub_module_list = []
 
         # module component groups
         self.joint_group = None
@@ -52,49 +69,35 @@ class TpModule(object):
         self.children_joint = None
 
         self.module_build_list = [
-            self.__create_module_top_group
+            self._create_module_top_group
         ]
 
     def build_module(self):
-        self.module_build_list.append(self.__pack_and_ship)
+        self.module_build_list.extend([
+            self._pack_and_ship,
+            self._create_sub_module_mirror_attributes])
 
         for function in self.module_build_list:
             function()
 
-    def __pack_and_ship(self):
-        for component_type in self.module_group_dict:
-            if self.module_group_dict[component_type]:
-                group = mc.group(self.module_group_dict[component_type],
-                                 name=self.tp_name.build(name=self.module_name,
-                                                         side=self.module_side,
-                                                         node_type=component_type,
-                                                         group=True))
-                self.module_group_list.append(group)
+    def set_name(self, name):
+        self.module_name = name
 
-        mc.parent(self.module_group_list, self.module_top_group)
+    def set_side(self, side):
+        self.module_side = side
 
-    def __create_module_control_node(self):
-        """
-        The control node holds the muscle_pull attribute, which will be used
-        to drive the system
-        """
-        self.module_control_node = mc.createNode('controller', name=self.tp_name.build(name=self.module_name,
-                                                                                       side=self.module_side,
-                                                                                       node_type='controller'))
-        mc.addAttr(self.module_control_node, longName='metadata', dataType='string')
+    def add_to_module_metadata(self):
+        pass
 
-    def __create_module_top_group(self):
-        self.module_top_group = mc.group(empty=True,
-                                         name=self.tp_name.build(name=self.module_name,
-                                                                 side=self.module_side,
-                                                                 node_type='module',
-                                                                 group=True))
+    # getters
+    def get_module_attribute_manager(self):
+        return self.module_attribute_manager
 
-        # self.module_top_group = TpModuleGroup(name=self.module_name,
-        #                                       side=self.module_side,
-        #                                       component_type='module')
-        # self.module_top_group.add_attribute_metadata()
-        # self.module_top_group.build()
+    def get_module_attribute_object_list(self):
+        return [attribute for attribute in self.module_attribute_manager.get_attribute_object_list()]
+
+    def get_module_metadata(self):
+        pass
 
     def get_module_top_group(self):
         return self.module_top_group
@@ -114,6 +117,10 @@ class TpModule(object):
     def get_joint_list(self):
         return self.joint_list
 
+    def get_sub_module_list(self):
+        pass
+
+    # multi purpose
     def list_available_data(self):
         pass
 
@@ -122,6 +129,74 @@ class TpModule(object):
 
     def append_to_module(self):
         pass
+
+    def _create_module_top_group(self):
+        self.module_top_group = mc.group(empty=True,
+                                         name=self.tp_name.build(name=self.module_name,
+                                                                 side=self.module_side,
+                                                                 node_type='module',
+                                                                 group=True))
+
+        self.module_attribute_manager = tpAttr.TpNodeAttributeManager(self.module_top_group)
+
+    def _create_sub_module_mirror_attributes(self):
+        """
+        If there are sub_modules on the list, gets attribute managers
+        :return:
+        """
+        if self.sub_module_list:
+            for sub_module in self.sub_module_list:
+                # get sub_module naming information
+                sub_module_name = sub_module.get_module_name()
+                sub_module_side = sub_module.get_module_side()
+
+                # add division
+                self.module_attribute_manager.add_division(sub_module_name)  # add module_side
+
+                # get attribute object list
+                sub_module_attribute_manager = sub_module.get_module_attribute_manager()
+                sub_module_attribute_list = sub_module_attribute_manager.get_attribute_object_list()
+
+                if sub_module_attribute_list:
+                    for attribute in sub_module_attribute_list:
+                        # transfer single attribute from sub_module to module
+                        attribute_name = attribute.get_attribute_name()
+                        parameters_dict = attribute.get_parameters_dict()
+
+                        module_attribute = self.module_attribute_manager.add_attribute(
+                            attribute_name,
+                            **parameters_dict)
+
+                        module_attribute.connect_to(attribute.get_node_dot_attribute())
+        else:
+            pass
+
+    def _connect_sub_module_attributes(self):
+        if self.sub_module_list:
+            for sub_module in self.sub_module_list:
+                pass
+
+    def _lock_and_hide_attributes(self):
+        pass
+
+    def _build_module_attributes(self):
+        pass
+
+    def _pack_and_ship(self):
+        """
+        Used to create and parent each component category to it's group.
+        Also parents the groups under the module top group
+        """
+        for component_type in self.module_group_dict:
+            if self.module_group_dict[component_type]:
+                group = mc.group(self.module_group_dict[component_type],
+                                 name=self.tp_name.build(name=self.module_name,
+                                                         side=self.module_side,
+                                                         node_type=component_type,
+                                                         group=True))
+                self.module_group_list.append(group)
+
+        mc.parent(self.module_group_list, self.module_top_group)
 
 
 class TpModuleGroupManager:
@@ -140,8 +215,9 @@ class TpModuleGroup:
 
     def __init__(self, name, side, component_type):
         self.group_name = name
-        self.group_component_type = component_type
         self.group_side = side
+        self.group_component_type = component_type
+
         self.item_list = []
         self.attribute_dict = {}
         self.attribute_queue_list = []
@@ -201,16 +277,6 @@ class TpModuleGroup:
                    maxValue=max_value)
 
         self.attribute_dict.update({long_name: '{group}.{attr}'.format(group=self.group_name, attr=long_name)})
-
-    def queue_add_attribute_double(self, long_name, keyable, default_value, min_value, max_value):
-        command = 'mc.addAttr({group}, longName={long_name}, keyable={keyable}, attributeType="double", ' \
-                  'defaultValue={def_value}, minValue={min}, maxValue={max})'.format(group=self.group_name,
-                                                                                     long_name=long_name,
-                                                                                     keyable=keyable,
-                                                                                     def_value=default_value,
-                                                                                     min=min_value,
-                                                                                     max=max_value)
-        self.attribute_queue_list.append(command)
 
     def add_attribute_float(self):
         pass
